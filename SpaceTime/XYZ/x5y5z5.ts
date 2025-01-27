991b31653770039077cff91c96b5b29f90b5b2bb
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-01-27 18:50:40 +0800

    Update x5y5z5.ts

--- SpaceTime/XYZ/x5y5z5.ts
+++ SpaceTime/XYZ/x5y5z5.ts
@@ -1,21 +1,20 @@
-
 function X5(){
-    for (let I = 0; I <= 4; ++I)
-       for (let J = 0; J <= 4; ++J)
-          for (let K = 0; K <= 4; ++K)
+    for (I = 0; I <= 4; ++I)
+       for (J = 0; J <= 4; ++J)
+          for (K = 0; K <= 4; ++K)
         break
 }
 function Y5() {
-    for (let L = 0; L <= -4 && L <= 4; ++L || L--)
-        for (let M = 0; M <= -4 && M <= 4; ++M || M--)
-            for (let N = 0; N <= -4 && N <= 4; ++N || N--)
+    for (L = 0; L <= -4 && L <= 4; ++L || L--)
+        for (M = 0; M <= -4 && M <= 4; ++M || M--)
+            for (N = 0; N <= -4 && N <= 4; ++N || N--)
         continue
 }
 
 function Z5(){
-    for (let O = 0; O >= -4; O--)
-       for (let P = 0; P >= -4; P--)
-          for (let Q = 0; Q >= -4; Q--)
+    for (O = 0; O >= -4; O--)
+       for (P = 0; P >= -4; P--)
+          for (Q = 0; Q >= -4; Q--)
         break
 }
 
